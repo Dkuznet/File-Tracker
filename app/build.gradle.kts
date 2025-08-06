95d78e6235a84bd67da0af30ba1446d3b0e0bd3b
Dmitrii <dkuznet@yandex.ru>
2025-08-06 22:22:46 +0300

    release v1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,16 @@
+import java.io.FileInputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
+
+val currentDate = SimpleDateFormat("yyyyMMdd").format(Date())
 
 android {
     namespace = "com.example.filetracker"
@@ -19,19 +26,44 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            val props = Properties()
+            val keystorePropsFile = rootProject.file("keystore.properties")
+
+            if (keystorePropsFile.exists()) {
+                props.load(FileInputStream(keystorePropsFile))
+
+                storeFile = file(props["storeFile"] as String)
+                storePassword = props["storePassword"] as String
+                keyAlias = props["keyAlias"] as String
+                keyPassword = props["keyPassword"] as String
+                storeType = "PKCS12"
+            } else {
+                println("⚠️ Warning: keystore.properties not found. Release signing will fail.")
+            }
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
+
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
     }
@@ -39,8 +71,8 @@ android {
     packaging {
         resources {
             excludes += "META-INF/LICENSE.md"
-            excludes += "META-INF/LICENSE-notice.md" // Optional, but good practice
-            excludes += "META-INF/NOTICE.md"        // Optional, but good practice
+            excludes += "META-INF/LICENSE-notice.md"
+            excludes += "META-INF/NOTICE.md"
         }
     }
 
@@ -57,6 +89,24 @@ kotlin {
     }
 }
 
+tasks.whenTaskAdded {
+    if (name == "assembleRelease") {
+        doLast {
+            val apkDir = layout.buildDirectory.dir("outputs/apk/release").get().asFile
+            if (apkDir.exists()) {
+                apkDir.listFiles()?.forEach { file ->
+                    if (file.name.endsWith(".apk")) {
+//                        val newName = "filetracker-v1.0-$currentDate.apk"
+                        val newName = "filetracker-v1.0.apk"
+                        file.renameTo(File(apkDir, newName))
+                    }
+                }
+            }
+        }
+    }
+}
+
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -68,11 +118,27 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.recyclerview)
     implementation(libs.androidx.junit.ktx)
+
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.room.testing)
+    testImplementation(libs.androidx.arch.core.testing)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.androidx.test.core)
+    testImplementation(libs.androidx.test.ext.junit)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.mockk)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.room.testing)
+    androidTestImplementation(libs.androidx.arch.core.testing)
+    androidTestImplementation(libs.mockito.android)
+    androidTestImplementation(libs.mockk.android)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
@@ -85,30 +151,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.constraintlayout)
-
-    // Material Components для MaterialButton
     implementation(libs.material)
     implementation(libs.androidx.datastore.preferences)
-
-    // Mockito and related test dependencies
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.mockito.kotlin)
-
-    // Room testing dependencies
-    testImplementation(libs.room.testing)
-    testImplementation(libs.androidx.arch.core.testing)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.androidx.test.core)
-    testImplementation(libs.androidx.test.ext.junit)
-
-    androidTestImplementation(libs.room.testing)
-    androidTestImplementation(libs.androidx.arch.core.testing)
-    androidTestImplementation(libs.mockito.android)
-
-    androidTestImplementation(libs.mockk.android)
-
-    // Robolectric для Android unit тестов
-    testImplementation(libs.robolectric)
-    testImplementation(libs.mockk)
-
 }
+
